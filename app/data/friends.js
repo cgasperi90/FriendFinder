6b2e6fb3a6646590b4369289c06cfcfbaca6de1b
Carlos Gasperi <cgasperi90@gmail.com>
2020-05-04 22:05:47 -0400

    adding all html and javascript code for app.

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friends = [
+    {
+        "name": "Lebron James",
+        "photo": "https://www.gstatic.com/tv/thumb/persons/287557/287557_v9_ba.jpg",
+        "scores": [
+            1,
+            5,
+            4,
+            5,
+            3,
+            2,
+            3,
+            1,
+            2,
+            2
+        ]
+    }
+]
+
+module.exports = {
+    friends: friends
+};
--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 //These are my dependencies
 var express = require("express");
 var path = require("path");
+var friends = require("./app/data/friends.js");
 
 ///Here is where we start the express server.
 var app = express();
-var PORT = 3000;
+var PORT = 3001;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+app.use(express.static("public/"));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
+
+app.get("/api/friends", function(req, res) {
+    return res.json(friends.friends);
+})
+
+app.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
+    friends.friends.push(newFriend);
+})
+
+
 app.listen(process.env.PORT || PORT, function() {
-    console.log("Listening on port: " + PORT);
+    console.log("Listening on port: http://localhost:" + PORT);
 })
 
 module.exports = {
     app: app,
     path: path
 }
+
+//I was not able to complete this assignment
+//I left out two things. I was not able to make the modal show up on the webpage and I was not able to get to the logic for the
+//page to compare the results from the user filling out the form to another user the previously filled out the survey.
